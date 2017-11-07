84a58717ba5aa59d97937e709796b32c74c5539d
Jari Balsma <jaribalsma@outlook.com>
2017-11-07 10:01:06 +0100

    Class pack/grid system

--- zonneschermCentrale.py
+++ zonneschermCentrale.py
@@ -5,19 +5,6 @@ import time, random
 def getCenterGeometry(width, height):
     return (width, height, int(tk.winfo_screenwidth() / 2) - int(width / 2), int(tk.winfo_screenheight() / 2) - int(height / 2))
 
-class PackFrame(Frame):
-    def __init__(self, master, width, height):
-        Frame.__init__(self, master)
-        self.master.title("zonneschermCentrale")
-        self.master.geometry('%dx%d+%d+%d' % getCenterGeometry(width, height))  # w,h,x,y
-
-        self.subFrame = Frame(self.master, width=width/3, bg='#FAA')
-        self.subFrame.pack(side=LEFT, fill=BOTH, expand=True)
-        self.label = Label(self.subFrame, text="Zonneschermen:")
-        self.label.pack(side=TOP, fill=BOTH)
-
-        self.subFrame2 = Frame(self.master, width=width/1, bg='#AAF').pack(side=RIGHT, fill=BOTH, expand=True)
-
 class GridFrame(Frame):
     def __init__(self, master, width, height):
         Frame.__init__(self, master)
@@ -42,30 +29,6 @@ class GridFrame(Frame):
 
 tk = Tk()
 
-#4mainFrame = PackFrame(tk, 640, 480)
 mainFrame = GridFrame(tk, 640, 480)
 
 tk.mainloop()
-
-
-
-
-
-
-
-'''canvas = Canvas(tk, width=640, height=480, bg='#000')
-canvas.pack()
-
-rect = canvas.create_rectangle(0,0,100,100, fill="#FFF")
-
-
-def GUI():
-    x = random.randrange(640)
-    y = random.randrange(480)
-    canvas.coords(rect, x, y, x + 100, y + 100)
-
-while(1):
-    GUI()
-    root.update_idletasks()
-    root.update()
-    time.sleep(0.025)'''
