55ce62ac9361b11d091a131d14eff8fa9359cca1
Jari Balsma <jaribalsma@outlook.com>
2017-11-09 23:36:30 +0100

    Test window, handshake, list serialconnections, request + data

--- zonneschermCentrale.py
+++ zonneschermCentrale.py
@@ -1,12 +1,19 @@
+from tkinter import *
 import serial, time
+
 
 class Protocol:
     def __init__(self):
         self.ser = None
 
-    def request(self, command):
+    def changeSer(self, ser):
+        self.ser = ser
+
+    def request(self, command, data=None):
         try:
             self.ser.write((command + "\n").encode('ascii'))
+            if data:
+                self.ser.write((data + "\n").encode('ascii'))
         except serial.SerialException:
             return None
 
@@ -20,14 +27,7 @@ class Protocol:
 
         return (test, info)
 
-    def handshake(self, comport):
-        try:
-            self.ser = serial.Serial(comport, 19200, timeout=4)
-            time.sleep(2)
-        except serial.SerialException:
-            print("No device in " + comport)
-            return False
-
+    def handshake(self):
         tries_left = 3
         while tries_left > 0:
             r = self.request("handshake")
@@ -40,7 +40,55 @@ class Protocol:
 
 
 
-comport = input("COM Poort:")
+comList = {}
+protocol = Protocol()
+tk = Tk()
+tk.columnconfigure(0, weight=3)
+tk.columnconfigure(1, weight=1)
+tk.rowconfigure(0, weight=1)
+tk.rowconfigure(1, weight=1)
+comInput = StringVar()
+inputField = Entry(tk, textvariable=comInput)
+inputField.grid(column=0, row=0, sticky=NSEW)
+
+def update():
+    for com in list(comList.keys()):
+        protocol.changeSer(comList.get(com))
+        if (protocol.request("ping") == ("OK", "pong")):
+            print(protocol.request("getName"))
+        else:
+            comList.get(com).close()
+            comList.pop(com)
+            print(com + " disconnected! :c")
+    tk.after(1000, update)
+update()
+
+def connect():
+    comport = comInput.get()
+    try:
+        ser = serial.Serial(comport, 19200, timeout=4)
+    except serial.SerialException:
+        print("Can't connect " + comport)
+        return None
+    time.sleep(2)
+    protocol.changeSer(ser)
+    if protocol.handshake():
+        comList[comport] = ser
+    else:
+        print("Handshake failed!")
+
+connectBtn = Button(tk, text='Connect', command=connect)
+connectBtn.grid(column=1, row=0, sticky=NSEW)
+
+
+
+tk.mainloop()
+
+
+
+
+
+'''comport = input("COM Poort:")
 protocol = Protocol()
 if (protocol.handshake(comport)):
     print(comport + " connected! =D")
@@ -48,6 +96,20 @@ if (protocol.handshake(comport)):
     while (1):
         if protocol.request("ping") != ("OK", "pong"):
             print(comport + " disconnected! :c")
-        time.sleep(1)
+
+        print("Get existing name: ", protocol.request("getName"))
+        time.sleep(4)
+        print("Get existing min and max temperature: ", protocol.request("getMinMaxTemp"))
+        time.sleep(4)
+
+        newName = input("New name: ")
+        print("Changing naam: ", protocol.request("setName", newName))
+        time.sleep(2)
+        print("Get new name: ", protocol.request("getName"))
+        time.sleep(2)
+        print("Set new min and max temperature: ", protocol.request("setMinMaxTemp"))
+        time.sleep(2)
+        print("Get new min and max temperature: ", protocol.request("getMinMaxTemp"))
+        time.sleep(6)
 else:
-    print("Handshake failed!")
+    print("Handshake failed!")'''
